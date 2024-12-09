0791700691d2dbdfd56bc6fb372a67e04e61b07e
CedricHermansBIT <cedric.hermans2@howest.be>
2024-12-09 20:56:56 +0100

    Entirely rework day 9 challenge 2 solution, now it works within a decent time. Harder than expected!

--- day9/challenge2.py
+++ day9/challenge2.py
@@ -3,61 +3,55 @@ with open("challengeinput.txt","r") as ifile:
     line=ifile.readline().strip()
 
 entire_sequence=[]
-length_per_id=dict()
 index=0
 file=True
-for i,c in enumerate(line):
+for c in line:
     if file:
-        entire_sequence+=[index]*int(c)
-        length_per_id[index]=int(c)
+        entire_sequence+=[(index,int(c))]
         index+=1
         file=False
     else:
-        entire_sequence+=["."]*int(c)
+        if c!="0":
+            entire_sequence+=[(-1,int(c))]
         file=True
 
-def calculate_missing(sequence):
-    keys=[]
-    length_per_missing={}
-    missing=0
-    for i,c in enumerate(sequence):
-        if c==".":
-            missing+=1
-        else:
-            if missing>0:
-                keys.append(i-missing)
-                length_per_missing[i-missing]=missing
-                missing=0
-    return keys,length_per_missing
-
-keys,length_per_missing=calculate_missing(entire_sequence)
-
-print(length_per_id)
-print(length_per_missing)
-
-for id_key in sorted(length_per_id.keys(),reverse=True):
-    print(id_key)
-    n=0
-    while n<len(length_per_missing.keys()):
-        missing_key=keys[n]  
-        i=entire_sequence.index(id_key)
-        if length_per_id[id_key]<=length_per_missing[missing_key] and i>missing_key:
-            # replace id_key in entire_sequence with "."s
-            for i,c in enumerate(entire_sequence):
-                if c==id_key:
-                    entire_sequence[i]="."
-            for i in range(length_per_id[id_key]):
-                entire_sequence[missing_key+i]=id_key
-            keys,length_per_missing=calculate_missing(entire_sequence)
-            #print(entire_sequence)
-            n=0
-        else:
-            n+=1
-            
-print(entire_sequence)
-
-total=0
-for i,c in enumerate(entire_sequence):
-    if c!=".":
-        total+=i*c
+#print(entire_sequence)
+pos_later=len(entire_sequence)-1
+while pos_later>0:
+#pos_later, (id_later, amount_later) in enumerate(entire_sequence[::-1]):
+    id_later, amount_later = entire_sequence[pos_later]
+    #pos_later=len(entire_sequence)-1-pos_later
+    
+    if id_later != -1:
+        #print(pos_later, id_later,amount_later)
+        for pos_earlier, (id_earlier,amount_earlier) in enumerate(entire_sequence):
+            if id_earlier == -1 and pos_later>pos_earlier:
+                if amount_earlier >= amount_later:
+                    # replace the first element with the second element
+                    entire_sequence[pos_earlier]=(id_later,amount_later)
+                    # replace the second element with missing (-1,value)
+                    entire_sequence[pos_later]=(-1,amount_later)
+                    # add remaining missing elements
+                    if amount_earlier != amount_later:
+                        entire_sequence=entire_sequence[:pos_earlier+1]+[(-1,amount_earlier-amount_later)]+entire_sequence[pos_earlier+1:]
+                        pos_later+=1
+                    #print(entire_sequence)
+                    break
+    pos_later-=1
+#print(entire_sequence)
+total=0       
+index=0
+for id,amount in entire_sequence:
+    if id != -1:
+        for i in range(amount):
+            print(id,end="")
+            #print(index,id)
+            total+=index*id
+            #print(total)
+            index+=1
+    else:
+        index+=amount
+        #print(index,0)
+        print("."*amount,end="")
+print()
 print(total)
